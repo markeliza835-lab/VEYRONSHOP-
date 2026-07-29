4738db45ae01dbca18976fd57a24bf8735f695d0
markeliza835-lab <mark.eliza835@gmail.com>
2026-07-29 17:02:50 +0700

    Update гитхаб.py

--- гитхаб.py
+++ гитхаб.py
@@ -3157,7 +3157,7 @@ def add_product(message):
 
 		    "price": int(price),
 
-		    "photo": "AgACAgIAAxkBAAIblWppzEj2mU2MhaEUdxxcRT69MaQAA9sXaxv6qFFLG-JP4cFKI-gBAAMCAAN5AAM9BA"
+		    "photo": "AgACAgIAAxkBAAIblmppzGwGn1A1BkNQLlahSv_HgrRjAALcF2sb-qhRS84QwWWhfpsFAQADAgADeQADPQQ"
 
 })
 
